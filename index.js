7a24ef2f5a9c6ba466584218c2d4a98876613410
Kibuchi3560 <jontezbuchiki3560@gmail.com>
2024-09-21 22:56:26 +0300

    Works on Looping

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+const gifts  = ['teddy bear', 'drone', 'doll'];
+
+function wrapGifts(array){
+    for(let i = 0; i < array.length; i++){
+        console.log(`Wrapped ${array[i]} and added a bow!`);
+        debugger;
+    }
+}
+
+function writeCards(array, eventName) {
+    let thankYouMessages = [];
+    for (let i = 0 ; i < array.length; i++){
+        const cardMessage = `Thank you, ${array[i]}, for the wonderful ${eventName} gift!`;
+        thankYouMessages.push(cardMessage);
+        console.log(cardMessage);
+    }
+    console.log(thankYouMessages);
+    return thankYouMessages;
+}
+let peopleToThank = ['Guandalupe', 'Ollie', 'Aki'];
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+
+    }
+}
+countDown(10)
